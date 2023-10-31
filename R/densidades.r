2c90c18b4d9eb1fb93d718adfcb5301082036078
prdm0 <pedro.rafael.marinho@gmail.com>
2023-10-31 13:17:04 -0300

    Densidade da média amostral de variáveis aleatórias gamma.

--- R/densidades.r
+++ R/densidades.r
@@ -1,23 +1,28 @@
-# Função densidade de probabilidade da média amostral de uma sequência de
-# variáveis aleatórias iids com distribuição \Gamma(\alpha, \beta). Temos   que
-# \overline{x} \sim \Gamma(n \alpha, \beta / n). Temos que \alpha > 0 e \beta >
-# 0, em que \alpha é parâmetro de forma e \beta é parâmetro de escala.
-densidade_gamma_xbarra <- function(x, mu, beta, ...) {
-  # Densidade de \overline{X} sem reparametrização(beta / n)^(n * alpha) * x^(n
-  # * alpha - 1) * exp(-beta * x / n) / gamma(n * alpha)
-  n <- length(x)
-  1 / gamma(n * mu) * (n * mu / beta)^(n * mu) * x^(n * mu - 1) * exp(-n * mu * x / beta)
+densidade_gamma_mean <- function(x, alpha, beta, n) {
+  alpha <- alpha / n
+  beta <- n * beta
+  beta^(n * alpha) / gamma(n * alpha) * x^(n * alpha - 1) * exp(-beta * x)
 }
 
-# Função densidade de probabilidade da média amostral de uma sequência de
-# variáveis aleatórias com distribuição Gaussiana Inversa (IG ~ (\mu, \lambda)).
-# x > 0, \mu > 0 e \lambda > 0, em que \mu é parâmetro de locação e \lambda é
-# parâmetro de forma. Temos que \overline{x} ~ IG(\mu, n\lambda).
-densidade_gaussiana_inversa <- function(x, mu, lambda, ...) {
-  n <- length(x)
-  lambda <- n * lambda
-  sqrt(lambda / (2 * pi * x^3)) * exp(-lambda * (x - mu)^2 / (2 * mu^2 * x))
+# Dados de uma v.a. com distribuição gamma
+random_gamma_mean <- function(n_sample, n, alpha, beta) {
+  observacoes_mean <- function() {
+    mean(rgamma(n = n, shape = alpha / n, rate = beta))
+  }
+  sapply(X = 1L:n_sample, FUN = \(i) observacoes_mean())
 }
 
-# integrate(densidade_gamma_xbarra, lower = 0, upper = 10, mu = 0.4, beta = 0.6)
-# integrate(densidade_gaussiana_inversa, lower = 0, upper = 100, mu = 1, lambda = 1)
+n <- 20
+
+dados <- random_gamma_mean(n_sample = 10000, n = n, alpha = 20, beta = 5)
+
+log_like_gamma <- function(par, x, n) {
+  fdp <- function(par, x) {
+    alpha <- par[1L]
+    beta <- par[2L]
+    densidade_gamma_mean(x = x, alpha = alpha, beta = beta, n = n)
+  }
+  -sum(log(fdp(par, x)))
+}
+
+optim(par = c(1.5, 1.5), fn = log_like_gamma, n = n, x = dados, method = "Nelder-Mead")
