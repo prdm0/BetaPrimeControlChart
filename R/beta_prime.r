f79c9118c288dffc5d2cab76b0f965e6dd26208f
prdm0 <pedro.rafael.marinho@gmail.com>
2023-11-25 22:18:05 -0300

    new file:   .Rbuildignore 	new file:   .Rhistory 	new file:   .gitignore 	new file:   BetaPrimeControlChart.Rproj 	new file:   DESCRIPTION 	new file:   NAMESPACE 	modified:   R/beta_prime.r 	deleted:    R/experimentar.r 	modified:   R/gamma.r

--- R/beta_prime.r
+++ R/beta_prime.r
@@ -1,136 +1,115 @@
-box::use(
-  extraDistr
-)
-
-#' @export
-d_xbarra_beta_prime <- function(x, mu, phi) {
-  n <- length(x)
-  x <- sum(x)
-
+d_xbarra_beta_prime <- function(x, n, mu, phi) {
   alpha <- mu * (phi + 1)
   beta <- phi + 2
 
   lambda <- n * alpha * (alpha + beta^2 - 2 * beta + n * alpha * beta - 2 * n * alpha + 1) / ((beta - 1) * (alpha + beta - 1))
   delta <- (2 * alpha + beta^2 - beta + n * alpha * beta - 2 * n * alpha) / (alpha + beta - 1)
 
-  n * extraDistr$dbetapr(x = x, shape1 = lambda, shape2 = delta)
+  n * extraDistr::dbetapr(x = n * x, shape1 = lambda, shape2 = delta)
 }
 
-#' @export
 r_beta_prime <- function(n_lotes, n, mu, phi, ...) {
   observacoes <- function() {
     alpha <- mu * (phi + 1)
     beta <- phi + 2
-    extraDistr$rbetapr(n = n, shape1 = alpha, shape2 = beta, ...)
+    extraDistr::rbetapr(n = n * n_lotes, shape1 = alpha, shape2 = beta, ...)
   }
-  lapply(X = 1L:n_lotes, FUN = \(i) observacoes())
+  m <- matrix(data = observacoes(), nrow = n_lotes, ncol = n, byrow = TRUE)
+  colnames(m) <- paste("n", 1L:ncol(m), sep = "_")
+  rownames(m) <- paste("sample", 1L:nrow(m), sep = "_")
+  return(m)
 }
 
 #' @export
-log_like_beta_prime <- function(par, x) {
-  mu <- par[1]
-  phi <- par[2]
-  -sum(log(d_xbarra_beta_prime(x = unlist(x), mu = mu, phi = phi)))
-}
+mle_beta_prime <-
+  function(data,
+           start = c(1, 1),
+           epsilon = 1e-6,
+           linesearch_algorithm = "LBFGS_LINESEARCH_BACKTRACKING_ARMIJO",
+           ...) {
+    x <- data
+    n <- ncol(x)
+    x <- apply(X = x, FUN = \(i) mean(i), MARGIN = 1L)
 
-qbp <- function(p, n, mu, phi, epsilon = 1e-4) {
-  fdp <- function(x, n, mu, phi) {
-    alpha1 <- mu * (phi + 1)
-    beta <- phi + 2
-
-    lambda <- n * alpha1 * (alpha1 + beta^2 - 2 * beta + n * alpha1 * beta - 2 * n * alpha1 + 1) / ((beta - 1) * (alpha1 + beta - 1))
-    delta <- (2 * alpha1 + beta^2 - beta + n * alpha1 * beta - 2 * n * alpha1) / (alpha1 + beta - 1)
-
-    n * extraDistr$dbetapr(x = x, shape1 = lambda, shape2 = delta)
-  }
-  repeat{
-    r <-
-      integrate(
-        f = function(x) fdp(x = n * x, n = n, mu = mu, phi = phi), lower = 0, upper = q
-      )$value
-    if (abs(r - p) < epsilon) {
-      return(q)
+    log_like_beta_prime <- function(x, n, mu, phi) {
+      -sum(log(d_xbarra_beta_prime(x = x, n = n, mu = mu, phi = phi)))
     }
-    q <- q + epsilon
+
+    grad_beta_prime <- function(x, n, mu, phi) {
+      numDeriv::grad(
+        func = \(par) log_like_beta_prime(x = x, n = n, mu = par[1L], phi = par[2L]),
+        x = c(mu, phi)
+      )
+    }
+
+    lbfgs::lbfgs(
+      call_eval = \(par) log_like_beta_prime(x = x, n = n, mu = par[1L], phi = par[2L]),
+      call_grad = \(par) grad_beta_prime(x = x, n = n, mu = par[1L], phi = par[2L]),
+      vars = start,
+      invisible = 1,
+      linesearch_algorithm = "LBFGS_LINESEARCH_BACKTRACKING_ARMIJO",
+      epsilon = 1e-6,
+      ...
+    )
   }
+
+qbp <- function(p, n, mu, phi) {
+  alpha1 <- mu * (phi + 1)
+  beta <- phi + 2
+  lambda <- n * alpha1 * (alpha1 + beta^2 - 2 * beta + n * alpha1 * beta - 2 * n * alpha1 + 1) / ((beta - 1) * (alpha1 + beta - 1))
+  delta <- (2 * alpha1 + beta^2 - beta + n * alpha1 * beta - 2 * n * alpha1) / (alpha1 + beta - 1)
+  extraDistr::qbetapr(p = p, shape1 = lambda, shape2 = delta) / n
 }
 
 qbp <- Vectorize(qbp, vectorize.args = "p")
 
-limites_beta_prime <- function(data = NULL, alpha = 0.0027, mu, phi) {
-  if (!is.list(data)) {
-    stop("O objeto dados deve ser uma lista.")
+limits_beta_prime <-
+  function(data = NULL,
+           alpha = 0.0027,
+           mu = NULL,
+           phi = NULL) {
+    if (!is.matrix(data)) {
+      stop("The object data must be a matrix!")
+    }
+
+    if (is.null(mu) || is.null(phi)) {
+      mle <- mle_beta_prime(data = data)
+      mu <- mle$par[1L]
+      phi <- mle$par[2L]
+    }
+
+    n <- ncol(data)
+
+    r <- qbp(p = c(alpha / 2, 1 - alpha / 2), n = n, mu = mu, phi = phi)
+    return(list(li = r[1L], ls = r[2L], mu_hat = mu, phi_hat = phi))
   }
 
-  n <- length(data[[1L]])
-
-  r <- qbp(p = c(alpha / 2, 1 - alpha / 2), n = n, mu = mu, phi = phi)
-  return(list(li = r[1L], ls = r[2L]))
-}
-
-peformance_beta_prime <- function(data, alpha = 0.0027, mu = 1, phi = 0.7) {
-  n <- data[[1L]] |> length()
-  vec_sum <- sapply(X = data, FUN = \(i) mean(i))
-  limites <- limites_beta_prime(data = data, alpha = alpha, mu = mu, phi = phi)
+stats_beta_prime <- function(data, alpha = 0.0027, mu = NULL, phi = NULL) {
+  if (!is.matrix(data)) {
+    stop("The object data must be a matrix!")
+  }
+  n <- ncol(data)
+  vec_sum <- apply(X = data, FUN = \(i) mean(i), MARGIN = 1L)
+  limites <- limits_beta_prime(data = data, alpha = alpha, mu = mu, phi = phi)
   fora <- sum(vec_sum < limites$li) + sum(vec_sum > limites$ls)
   alpha_hat <- fora / length(vec_sum)
-  list(alpha_hat = alpha_hat, arl0 = 1 / alpha_hat)
+  list(
+    mu_hat = limites$mu_hat,
+    phi_hat = limites$phi_hat,
+    alpha_hat = alpha_hat,
+    ARL = 1 / alpha_hat,
+    MRL = log(0.5) / log(1 - alpha_hat),
+    SDRL = sqrt((1 - alpha_hat) / (alpha_hat^2)),
+    li = limites$li,
+    ls = limites$ls
+  )
 }
 
 set.seed(0)
-dados <- r_beta_prime(n_lotes = 100e3L, n = 1000, mu = 1, phi = 1.7)
-peformance_beta_prime(data = dados, alpha = 0.0027, mu = 1, phi = 1.7)
+dados <- r_beta_prime(n_lotes = 10e3L, n = 2050, mu = 1, phi = 1.7)
 
-# #' @export
-# limites_beta_prime <- function(dados = NULL, n = NULL, alpha = 0.0027, mu, phi) {
-#   if (!is.null(dados) && is.list(dados)) {
-#     n <- length(dados[[1L]])
-#   } else {
-#     if (is.null(n)) {
-#       stop("Dados ou n devem ser fornecidos")
-#     }
-#   }
+# Estimando os parâmetros por maxima verossimilhança
+stats_beta_prime(data = dados, alpha = 0.0027)
 
-#   qbp <- function(p, n, mu, phi) {
-#     fdp <- function(x, n, mu, phi) {
-#       alpha1 <- mu * (phi + 1)
-#       beta <- phi + 2
-
-#       lambda <- n * alpha1 * (alpha1 + beta^2 - 2 * beta + n * alpha1 * beta - 2 * n * alpha1 + 1) / ((beta - 1) * (alpha1 + beta - 1))
-#       delta <- (2 * alpha1 + beta^2 - beta + n * alpha1 * beta - 2 * n * alpha1) / (alpha1 + beta - 1)
-
-#       n * extraDistr$dbetapr(x = x, shape1 = lambda, shape2 = delta)
-#     }
-#     q <- 1e-4
-#     repeat{
-#       r <-
-#         integrate(
-#           f = function(x) fdp(x = x, n = n, mu = mu, phi = phi), lower = 0, upper = q
-#         )$value
-#       if (abs(r - p) < 1e-4) {
-#         return(q)
-#       }
-#       q <- q + 1e-4
-#     }
-#   }
-
-#   qbp <- Vectorize(qbp, vectorize.args = "p")
-
-#   r <- qbp(p = c(alpha / 2, 1 - alpha / 2), n = n, mu = mu, phi = phi)
-#   return(list(li = r[1L], ls = r[2L]))
-# }
-
-# dados <- r_beta_prime(n_lotes = 1000, n = 100, mu = 0.6, phi = 0.7)
-
-
-# fora <- function(dados, alpha = 0.0027, mu = 0.6, phi = 0.7) {
-#   vec_mean <- sapply(X = dados, FUN = mean)
-
-#   n <- length(dados[[1L]])
-
-#   limites <- limites_beta_prime(dados = dados, alpha = alpha, mu = mu, phi = phi)
-#   return(sum(vec_mean < limites[[1L]] | vec_mean > limites[[2L]]))
-# }
-
-# fora(dados = dados, alpha = 0.0027, mu = 0.6, phi = 0.7)
-# limites_beta_prime(dados = dados, alpha = 0.0027, mu = 1, phi = 0.7)
+# Utilizando os parametros verdadeiros
+stats_beta_prime(data = dados, alpha = 0.0027, mu = 1, phi = 1.7)
--- R/gamma.r
+++ R/gamma.r
@@ -1,10 +1,4 @@
-# Distribuição Gamma A densidade da variavel aleatoria x barra quando x segue a
-# distribuicao gamma com k e theta, em que k eh parametro de forma e theta eh
-# escala.
-#' @export
-d_xbarra_gamma <- function(x, mu, k) {
-  n <- length(x)
-  x <- mean(x)
+d_xbarra_gamma <- function(x, n, mu, k) {
   dgamma(x = x, shape = n * k, scale = mu / (n * k))
 }
 
@@ -13,38 +7,94 @@ d_xbarra_gamma <- function(x, mu, k) {
 #' @export
 r_gamma <- function(n_lotes, n, mu, k, ...) {
   observacoes <- function() {
-    rgamma(n = n, shape = k, scale = mu / k, ...)
+    rgamma(n = n * n_lotes, shape = k, scale = mu / k, ...)
   }
-  lapply(X = 1L:n_lotes, FUN = \(i) observacoes())
+  m <- matrix(data = observacoes(), nrow = n_lotes, ncol = n, byrow = TRUE)
+  colnames(m) <- paste("n", 1L:ncol(m), sep = "_")
+  rownames(m) <- paste("sample", 1L:nrow(m), sep = "_")
+  return(m)
 }
 
 #' @export
-log_like_gamma <- function(par, x) {
-  fdp <- function(par, x) {
-    mu <- par[1L]
-    k <- par[2L]
-    d_xbarra_gamma(x = unlist(x), mu = mu, k = k)
+mle_gamma <-
+  function(data,
+           start = c(1, 1),
+           epsilon = 1e-6,
+           linesearch_algorithm = "LBFGS_LINESEARCH_BACKTRACKING_ARMIJO",
+           ...) {
+    x <- data
+    n <- ncol(x)
+    x <- apply(X = x, FUN = \(i) mean(i), MARGIN = 1L)
+
+    log_like_gamma <- function(x, n, mu, k) {
+      -sum(log(d_xbarra_gamma(x = x, n = n, mu = mu, k = k)))
+    }
+
+    grad_gamma <- function(x, n, mu, k) {
+      numDeriv::grad(
+        func = \(par) log_like_gamma(x = x, n = n, mu = par[1L], k = par[2L]),
+        x = c(mu, k)
+      )
+    }
+
+    lbfgs::lbfgs(
+      call_eval = \(par) log_like_gamma(x = x, n = n, mu = par[1L], k = par[2L]),
+      call_grad = \(par) grad_gamma(x = x, n = n, mu = par[1L], k = par[2L]),
+      vars = start,
+      invisible = 1,
+      linesearch_algorithm = "LBFGS_LINESEARCH_BACKTRACKING_ARMIJO",
+      epsilon = 1e-6,
+      ...
+    )
   }
-  -sum(log(fdp(par, x)))
-}
 
-#' @export
-limites_gamma <- function(dados = NULL, alpha = 0.0027, mu, k) {
-  if (!is.list(dados)) {
-    stop("O objeto dados deve ser uma lista.")
+limits_gamma <-
+  function(data = NULL,
+           alpha = 0.0027,
+           mu = NULL,
+           k = NULL) {
+    if (!is.matrix(data)) {
+      stop("The object data must be a matrix!")
+    }
+
+    if (is.null(mu) || is.null(k)) {
+      mle <- mle_gamma(data = data)
+      mu <- mle$par[1L]
+      k <- mle$par[2L]
+    }
+
+    n <- ncol(data)
+
+    r <- qgamma(p = c(alpha / 2, 1 - alpha / 2), shape = n * k, scale = mu / (n * k))
+    return(list(li = r[1L], ls = r[2L], mu_hat = mu, k_hat = k))
   }
-  n <- length(dados[[1]])
-  r <- qgamma(p = c(alpha / 2, 1 - alpha / 2), shape = n * k, scale = mu / (n * k))
-  return(list(li = r[1L], ls = r[2L]))
+
+stats_gamma <- function(data, alpha = 0.0027, mu = NULL, k = NULL) {
+  if (!is.matrix(data)) {
+    stop("The object data must be a matrix!")
+  }
+  n <- ncol(data)
+  vec_sum <- apply(X = data, FUN = \(i) mean(i), MARGIN = 1L)
+  limites <- limits_gamma(data = data, alpha = alpha, mu = mu, k = k)
+  fora <- sum(vec_sum < limites$li) + sum(vec_sum > limites$ls)
+  alpha_hat <- fora / length(vec_sum)
+  list(
+    mu_hat = limites$mu_hat,
+    k_hat = limites$k_hat,
+    alpha_hat = alpha_hat,
+    ARL = 1 / alpha_hat,
+    MRL = log(0.5) / log(1 - alpha_hat),
+    SDRL = sqrt((1 - alpha_hat) / (alpha_hat^2)),
+    li = limites$li,
+    ls = limites$ls
+  )
 }
 
-arl0_gamma <- function(dados, alpha = 0.0027, mu = 1, k = 0.7) {
-  vec_mean <- sapply(X = dados, FUN = mean)
-  limites <- limites_gamma(dados = dados, alpha = alpha, mu = mu, k = k)
-  fora <- sum(vec_mean < limites$li) + sum(vec_mean > limites$ls)
-  alpha_hat <- fora / length(vec_mean)
-  list(alpha_hat = alpha_hat, arl0 = 1 / alpha_hat)
-}
+set.seed(0)
+dados <- r_gamma(n_lotes = 10000, n = 100, mu = 1, k = 1.7)
 
-dados <- r_gamma(n_lotes = 100e3L, n = 50L, mu = 1, k = 1.7)
-arl0_gamma(dados = dados, alpha = 0.0027, mu = 1, k = 1.7)
+# Estimando os parâmetros por maxima verossimilhança
+stats_gamma(data = dados, alpha = 0.0027)
+
+# Utilizando os parametros verdadeiros
+stats_gamma(data = dados, alpha = 0.0027, mu = 1, k = 1.7)
